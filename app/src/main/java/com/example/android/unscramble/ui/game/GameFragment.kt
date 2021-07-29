abee3835979b76a9982ffbaa1698e6a3e77db6bb
tuxedotshirt <don.hagen@hotmail.com>
2021-07-29 01:34:10 -0600

    Store data in ViewModel step 8 complete

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
 import com.example.android.unscramble.databinding.GameFragmentBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 /**
  * Fragment where the game is played, contains the game logic.
@@ -74,7 +75,12 @@ class GameFragment : Fragment() {
         * Displays the next scrambled word.
         */
     private fun onSubmitWord() {
-
+        if(viewModel.nextWord()){
+            updateNextWordOnScreen()
+        }
+        else{
+            showFinalScoreDialog()
+        }
     }
 
     /*
@@ -128,5 +134,18 @@ class GameFragment : Fragment() {
      */
     private fun updateNextWordOnScreen() {
         binding.textViewUnscrambledWord.text = viewModel.currentScrambledWord
+    }
+
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext()).setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setCancelable(true)
+            .setNegativeButton(getString(R.string.exit)) {
+                _,_ -> exitGame()
+                }
+            .setPositiveButton(getString(R.string.play_again)) {
+                _,_ -> restartGame()
+                }
+            .show()
     }
 }
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,13 +5,15 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    private var score = 0
     private var currentWordCount = 0
     private lateinit var _currentScrambledWord: String
     private var wordsList: MutableList<String> = mutableListOf()
     private lateinit var currentWord: String
 
     val currentScrambledWord: String get() = _currentScrambledWord
+
+    private var _score = 0
+    val score: Int get() = _score
 
     init{
         Log.d("GameFragment", "GameViewModel created!")
