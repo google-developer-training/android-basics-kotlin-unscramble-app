a28488a62cfcbc003cb392c7c110d816c18fbd76
tuxedotshirt <don.hagen@hotmail.com>
2021-07-28 22:52:49 -0600

    Store data in ViewModel Step 7

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -6,15 +6,41 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
     init{
         Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
     }
     private var score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord: String
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
 
     val currentScrambledWord: String get() = _currentScrambledWord
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (tempWord.toString().equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        Log.d("currentWord", currentWord)
+        if(wordsList.isEmpty())
+            Log.d("wordsList", "empty")
+        else{
+            Log.d("wordsList", "not empty")
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordsList.add(currentWord)
+        }
     }
 }
