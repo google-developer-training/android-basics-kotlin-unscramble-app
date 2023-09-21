26f0200ee9e2f36371ed2fbeeaf0bc7c023783a6
ruly <rohmatchoirulyanwar@gmail.com>
2023-09-21 21:17:55 +0700

    menambahkan onCreatedView

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -47,6 +47,7 @@ class GameFragment : Fragment() {
     ): View {
         // Inflate the layout XML file and return a binding object instance
         binding = GameFragmentBinding.inflate(inflater, container, false)
+        Log,d("GameFragment","GameFragment created/re-created! ")
         return binding.root
     }
 
