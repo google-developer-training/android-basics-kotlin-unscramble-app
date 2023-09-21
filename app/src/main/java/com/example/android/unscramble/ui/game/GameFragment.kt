20e7c512c7f06c65f25b2561d3055e7e9355f609
ruly <rohmatchoirulyanwar@gmail.com>
2023-09-21 21:21:51 +0700

    Merubah metode callback onDetach()

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -115,6 +115,13 @@ class GameFragment : Fragment() {
         activity?.finish()
     }
 
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("GameFragment", "GameFragment destroyed!")
+    }
+
+
+
     /*
     * Sets and resets the text field error status.
     */
