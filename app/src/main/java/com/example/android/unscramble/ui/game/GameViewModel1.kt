be04234cd3c9b01766ef7e38a026d6c06ebcb10c
icachintyas <aicachintya@gmail.com>
2023-09-25 14:42:56 +0700

    Memverifikasi View Model dan mempertahankan data

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel1.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel1.kt
@@ -7,6 +7,10 @@ class GameViewModel1 :  ViewModel(){
     private var _score = 0
     val score: Int
         get() = _score
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
     init {
         Log.d("GameFragment", "GameViewModel created!")
         getNextWord()
