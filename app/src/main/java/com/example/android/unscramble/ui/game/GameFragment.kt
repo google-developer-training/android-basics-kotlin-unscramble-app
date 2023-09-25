ae473eb91c537ea792077c64c8ddbfd43fb520d8
icachintyas <aicachintya@gmail.com>
2023-09-25 14:18:02 +0700

    Memperbarui kolom teks

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -71,13 +71,19 @@ class GameFragment : Fragment() {
     * Displays the next scrambled word.
     */
     private fun onSubmitWord() {
+        val playerWord = binding.textInputEditText.text.toString()
+
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
             if (viewModel.nextWord()) {
                 updateNextWordOnScreen()
             } else {
                 showFinalScoreDialog()
             }
+        } else {
+            setErrorTextField(true)
         }
-
+    }
     /*
      * Skips the current word without changing the score.
      * Increases the word count.
