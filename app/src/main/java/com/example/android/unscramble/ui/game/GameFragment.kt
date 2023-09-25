e28d72c7b8af16b5f612e40e9bbdc97fb6c10da2
Mimingming10 <natasyain10@gmail.com>
2023-09-25 13:34:37 +0700

    Menambahkan resource string untuk judul pada dialog pemberitahuan

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -106,6 +106,7 @@ class GameFragment : Fragment() {
     */
     private fun showFinalScoreDialog() {
         MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
     }
 
     /*
