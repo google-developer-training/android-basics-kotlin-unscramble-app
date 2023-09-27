e258a0860997ed46c4594ef0fb5dc72feb25de1d
Mimingming10 <natasyain10@gmail.com>
2023-09-27 17:53:00 +0700

    Mengubah variabel _currentScrambledWord menjadi MutableLiveData<String> serta mengubah  variabel _currentScrambledWord menjadi val

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel1.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel1.kt
@@ -1,15 +1,18 @@
 package com.example.android.unscramble.ui.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel1 :  ViewModel(){
-    private var _score = 0
-    val score: Int
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int>
         get() = _score
 
-    private var _currentWordCount = 0
-    val currentWordCount: Int
+
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
         get() = _currentWordCount
 
     private lateinit var _currentScrambledWord: String
