258aa6fab515774c281a7c1f556c5df696169d4c
ruly <rohmatchoirulyanwar@gmail.com>
2023-09-29 13:23:46 +0700

    menggunakan fungsi Kotlin inc() untuk meningkatkan nilai satu per satu dengan keamanan null.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel1.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel1.kt
@@ -41,7 +41,7 @@ class GameViewModel1 :  ViewModel(){
         }
         if (wordsList.contains(currentWord)) {
             getNextWord()
-        } else {
+        }  else {
             _currentScrambledWord.value = String(tempWord)
             _currentWordCount.value = (_currentWordCount.value)?.inc()
             wordsList.add(currentWord)
