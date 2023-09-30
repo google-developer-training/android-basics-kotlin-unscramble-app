4d27eb0029b341b0572cb933a4c1911bfd5b8dce
Mimingming10 <natasyain10@gmail.com>
2023-09-30 18:16:07 +0700

    Commit akhir 4

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -16,7 +16,6 @@
 
 package com.example.android.unscramble.ui.game
 
-import android.app.ProgressDialog.show
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.viewModels
@@ -64,25 +63,19 @@ class GameFragment : Fragment() {
         binding.maxNoOfWords = MAX_NO_OF_WORDS
 
         // Setup a click listener for the Submit and Skip buttons.
-        binding.submit.setOnClickListener { onSubmitWord() }
-        binding.skip.setOnClickListener { onSkipWord() }
         // Update the UI
         // Specify the fragment view as the lifecycle owner of the binding.
         // This is used so that the binding can observe LiveData updates
         binding.lifecycleOwner = viewLifecycleOwner
+
         binding.submit.setOnClickListener { onSubmitWord() }
         binding.skip.setOnClickListener { onSkipWord() }
         if (viewModel.isGameOver()) { showFinalScoreDialog() }
-
-
-
 
         // Observe the currentScrambledWord LiveData.
         //viewModel.currentScrambledWord.observe()
 
         //Observe the scrambledCharArray LiveData, passing in the LifecycleOwner and the observer.
-
-
 
     }
 
